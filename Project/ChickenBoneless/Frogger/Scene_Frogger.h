b27a6365301d7dda07e40a229d85bd946589bad0
Teerapath-Keawratana <tkeawratana@gmail.com>
2025-02-11 10:52:34 -0400

    Finished all texture and working on Animation

---
+++ Project/ChickenBoneless/Frogger/Scene_Frogger.h
@@ -0,0 +1,85 @@
+//
+// Created by David Burchill on 2023-09-27.
+//
+
+#pragma once
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics.hpp>
+#include "EntityManager.h"
+#include "Entity.h"
+#include "Scene.h"
+#include "GameEngine.h"
+
+struct PlayerConfig { int  FR, FG, FB, OR, OG, OB, V; float  SR, CR, OT, S, AS; };
+struct EnemyConfig { int  OR, OG, OB, VMIN, VMAX, L, SI; float  SR, CR, OT, SMIN, SMAX; };
+struct BulletConfig { float SR, CR, S; };
+
+class Scene_Frogger : public Scene {
+
+	sPtrEntt				_player{ nullptr };
+	sPtrEntt				_cat{ nullptr };
+	sPtrEntt				_human{ nullptr };
+	sPtrEntt				_dog{ nullptr };
+	sPtrEntt				_bullet{ nullptr };
+
+	BulletConfig			_bulletConfig;
+
+	sf::FloatRect           _worldBounds;
+
+	int						_lives{ 5 };
+	int						_score{ 0 };
+	int						_scoreTotal{ 0 };
+	int						_scoredHeights[11] = {};
+	int						_winningScore{ 550 };
+
+	bool					_isFinish = false;
+	bool					_isComplete = false;
+	bool                    _drawTextures{ true };
+	bool                    _drawAABB{ false };
+	bool                    _drawCam{ false };
+	float					_timer = 90.f;
+	const float				_timerThreshold = 90.f;
+
+
+	//systems
+	void                    sMovement(sf::Time dt);
+	void                    sCollisions();
+	void                    sUpdate(sf::Time dt);
+	void	                onEnd() override;
+	void                    sSpawnMovingEntities();
+	void					sAnimation(sf::Time dt);
+	void					drawScore();
+	void					getScore();
+	void					drawLife();
+	void					spawnLife();
+	void					drawGameOver();
+	void					drawTimer();
+	void					drawWin();
+
+
+
+	// helper functions
+	void                    checkVehicleCollision();
+	void                    checkRiverCollision();
+	void                    init(const std::string& path);
+	void                    loadLevel(const std::string& path);
+	void                    spawnPlayer(sf::Vector2f pos);
+	void                    playerMovement(sf::Time dt);
+	void                    adjustPlayerPosition();
+	void                    spawnEnemyVehicle();
+	void                    adjustVehiclePosition();
+	void                    spawnRiverEntities();
+	void                    adjustRiverEntityPosition();
+	void	                registerActions();
+	void                    checkIfDead(sPtrEntt e);
+	void					spawnBullet(sf::Vector2f mPos);
+
+public:
+	Scene_Frogger(GameEngine* gameEngine, const std::string& levelPath);
+
+	void		update(sf::Time dt) override;
+	void		sDoAction(const Command& action) override;
+	void		sRender() override;
+
+};
---
+++ Project/README.md
@@ -0,0 +1,2 @@
+# Project
+Chicken Boneless Game Project
